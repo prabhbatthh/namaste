5822dc2684b2913fd31ed4f2140ff2f9d13334b0
Prabhjot Kaur <pkbatth@Prabhjots-MacBook-Air.local>
2024-09-19 11:34:54 -0700

    episode1

---
+++ app.js
@@ -0,0 +1,18 @@
+/*const heading = React.createElement("h1",{id: "heading"} , "Hello World from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);*/
+
+const parent = React.createElement("div",{id:"parent"},
+    [React.createElement("div",{id:"child1"},
+        [React.createElement("h1",{},"iam a h1 tag",),
+        React.createElement("h2",{},"iam a h2 tag",)]
+   ),
+    React.createElement("div",{id:"child2"},
+        [React.createElement("h1",{},"iam a h1 tag",),
+        React.createElement("h2",{},"iam a h2 tag",)]
+)]
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+  
